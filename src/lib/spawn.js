4a5430c6db044cf929fe2f79276e230364dc3744
Marius Lundgård <studio@mariuslundgard.com>
2018-01-23 14:43:34 +0100

    fix(monorepo): Fix output rendering

--- src/lib/spawn.js
+++ src/lib/spawn.js
@@ -6,24 +6,43 @@ const nodeSpawn = require('child_process').spawn
 
 module.exports = function spawn (scope, script, args, opts) {
   return new Promise((resolve, reject) => {
-    const p = nodeSpawn(script, args, {cwd: opts.cwd})
+    const p = nodeSpawn(script, args, { cwd: opts.cwd })
+
+    let outputWritten = false
 
     p.stdout.on('data', data => {
       if (!opts.quiet) {
-        process.stdout.write(`${chalk.blue(scope)} ${data.toString()}`)
+        let prefix = ''
+        if (!outputWritten) {
+          prefix = chalk.green(scope) + ' '
+          outputWritten = true
+        }
+        process.stdout.write(
+          prefix +
+            data.toString().replace(/\n/g, '\n' + chalk.green(scope) + ' ')
+        )
       }
     })
 
     p.stderr.on('data', data => {
       if (!opts.quiet) {
-        process.stderr.write(`${chalk.blue(scope)} ${data.toString()}`)
+        let prefix = ''
+        if (!outputWritten) {
+          prefix = chalk.red(scope) + ' '
+          outputWritten = true
+        }
+        process.stderr.write(
+          prefix + data.toString().replace(/\n/g, '\n' + chalk.red(scope) + ' ')
+        )
       }
     })
 
     p.on('exit', code => {
       if (code === 0) {
+        process.stdout.write('OK' + '\n')
         resolve()
       } else {
+        process.stderr.write('code ' + code + '\n')
         const err = new Error(`Script failure`)
         err.code = code
         err.scope = scope
