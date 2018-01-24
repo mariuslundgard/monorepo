dd981141ececa9b085dc0c5d1b9f31520490ef79
Marius Lundgård <studio@mariuslundgard.com>
2018-01-25 00:44:14 +0100

    refactor(monorepo): Create a transform stream for prefixing output

--- src/lib/spawn.js
+++ src/lib/spawn.js
@@ -3,47 +3,31 @@
 const Promise = require('bluebird')
 const chalk = require('chalk')
 const nodeSpawn = require('child_process').spawn
+const prefixedStream = require('./prefixedStream')
 
 module.exports = function spawn (scope, script, args, opts) {
   return new Promise((resolve, reject) => {
-    const p = nodeSpawn(script, args, { cwd: opts.cwd })
+    const p = nodeSpawn(script, args, {cwd: opts.cwd})
 
-    let outputWritten = false
+    if (!opts.quiet) {
+      const stderr = prefixedStream.create({prefix: chalk.red(scope) + ' '})
+      const stdout = prefixedStream.create({prefix: chalk.green(scope) + ' '})
 
-    p.stdout.on('data', data => {
-      if (!opts.quiet) {
-        let prefix = ''
-        if (!outputWritten) {
-          prefix = chalk.green(scope) + ' '
-          outputWritten = true
-        }
-        process.stdout.write(
-          prefix +
-            data.toString().replace(/\n/g, '\n' + chalk.green(scope) + ' ')
-        )
-      }
-    })
-
-    p.stderr.on('data', data => {
-      if (!opts.quiet) {
-        let prefix = ''
-        if (!outputWritten) {
-          prefix = chalk.red(scope) + ' '
-          outputWritten = true
-        }
-        process.stderr.write(
-          prefix + data.toString().replace(/\n/g, '\n' + chalk.red(scope) + ' ')
-        )
-      }
-    })
+      p.stderr.pipe(stderr).pipe(process.stderr)
+      p.stdout.pipe(stdout).pipe(process.stdout)
+    }
 
     p.on('exit', code => {
       if (code === 0) {
-        process.stdout.write('OK' + '\n')
+        if (!opts.quiet) {
+          console.log(chalk.green(scope) + ' OK')
+        }
         resolve()
       } else {
-        process.stderr.write('code ' + code + '\n')
         const err = new Error(`Script failure`)
+        if (!opts.quiet) {
+          console.error(chalk.red(scope) + ' exit code #' + code)
+        }
         err.code = code
         err.scope = scope
         reject(err)
---
+++ test/lib/prefixedStream.spec.js
@@ -0,0 +1,48 @@
+'use strict'
+
+const Stream = require('stream')
+const prefixedStream = require('../../src/lib/prefixedStream')
+
+function createMockReadStream () {
+  const s = new Stream()
+  s.readable = true
+  return s
+}
+
+function createMockWriteStream () {
+  const s = new Stream.Writable()
+  s._data = ''
+  s._write = function (chunk, encoding, done) {
+    s._data += chunk.toString()
+    done()
+  }
+  return s
+}
+
+describe('lib/prefixedStream', () => {
+  it('should prefix each line in a stream', done => {
+    const s = prefixedStream.create({prefix: 'foo > '})
+    const mockSubStdout = createMockReadStream()
+    const mockStdout = createMockWriteStream()
+
+    mockSubStdout.pipe(s).pipe(mockStdout)
+
+    mockStdout.on('data', str => {
+      console.log(str)
+    })
+
+    mockStdout.on('finish', () => {
+      expect(mockStdout._data).toEqual(
+        'foo > abcdefghijklmnopqrstuvwxyz\nfoo > abc'
+      )
+      done()
+    })
+
+    mockSubStdout.emit('data', 'abc')
+    mockSubStdout.emit('data', 'defghi')
+    mockSubStdout.emit('data', 'jklmno')
+    mockSubStdout.emit('data', 'pqrstuvwxyz\n')
+    mockSubStdout.emit('data', 'abc')
+    mockSubStdout.emit('end')
+  })
+})
